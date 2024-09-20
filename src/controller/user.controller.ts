1df69fb59e0c4cff72c0ca0e04eac10ff37e11c5
Willian Giovanini Dei Tosi <104787632+williandeitosi@users.noreply.github.com>
2024-09-20 17:35:51 +0000

    feat: create test for register, verify format

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,35 @@
 import { Request, Response } from "express";
 import { prisma } from "../app";
 
-export const register = async (req: Request, res: Response) => {
-    const userData = req.body
-
-    await prisma.user.create({data: userData})
-
-    res.status(200).send({message: 'User registered successfully'})
+interface TypeData {
+    id: number;
+    email: string;
+    password: string;
+    name: string;
 }
+
+export const register = async (req: Request, res: Response) => {
+    const userData: TypeData = req.body;
+
+    try {
+        if (typeof userData.email !== 'string' || typeof userData.password !== 'string') {
+            return res.status(400).send({ message: 'Invalid Email and Password format' })
+        }
+
+        const emailExists = await prisma.user.findUnique({
+            where: { email: userData.email }
+        });
+
+        if (emailExists) {
+            return res.status(400).send({ message: 'User already exists' });
+        }
+
+
+        await prisma.user.create({ data: userData });
+
+        res.status(200).send({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Internal server error' });
+    }
+};
--- src/tests/user.controller.spec.ts
+++ src/tests/user.controller.spec.ts
@@ -1,11 +1,35 @@
 import request from 'supertest'
 import app from '../server'
+import { prisma } from '../app'
 
 describe('register user', () => {
-    it('should return status code 200 for successful registration', async () => {
-        const response = await request(app).post('/register').send({ name: 'willtest', password: 'passtest' })
+    beforeEach(async () => {
+        await prisma.user.deleteMany()
+    })
 
+    it('Should verify Email and password formt invalid', async () => {
+        const response = await request(app).post('/register').send({ name: 'willtest', password: 123 , email: 'test@email.com' })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe('Invalid Email and Password format')
+    })
+
+
+    it('should return status code 200 for successful registration', async () => {
+        const response = await request(app).post('/register').send({ name: 'willtest', password: 'passtest', email: 'test@email.com' })
+        
         expect(response.status).toBe(200)
         expect(response.body.message).toBe('User registered successfully')
     })
+    
+    it('Should return status 400 for user is already exists', async () => {
+        await request(app).post('/register').send({ name: 'willtest', password: 'passtest', email: 'test@email.com' })
+
+        const response = await request(app).post('/register').send({ name: 'willtest', password: 'passtest', email: 'test@email.com' })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe('User already exists')
+    })
+
+
 })
